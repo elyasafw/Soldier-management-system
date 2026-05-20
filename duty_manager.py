2c77f3c308ad11207af1b641e4f50c648ffde870
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-20 12:18:22 +0300

    Adding files: until.py soldier_manager.py duty_manager.py with functions divided into files

---
+++ duty_manager.py
@@ -0,0 +1,10 @@
+def add_duty(soldier_id: int, duty_name: str, day: str):
+    pass
+
+
+def update_duty_status(soldier_id: int, duty_name: str, new_status: str):
+    pass
+
+
+def get_soldier_duties(soldier_id: int):
+    pass
