a93384afd44e36537afaa9253210f21ac137cdaf
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-20 12:37:24 +0300

    Adding a data file to the system data

---
+++ data.py
@@ -0,0 +1,14 @@
+Soldiers = {}
+
+duties = {
+    "1": "keeping",
+    "2": "tour",
+    "3": "kitchen",
+    "4": "cleanliness",
+    "5": "Class attendant",
+    "6": "logistics"
+    }
+
+days = ["sunday", "monday", "tuesday", "wednesday", "thursday"]
+
+statuses = ["pending", "completed", " missed"]
