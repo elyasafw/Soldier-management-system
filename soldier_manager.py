0993e120e8fe515eb2419ca95709c32f8ad6f6b8
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-20 16:36:17 +0300

    Adding content of add_soldier ​​function with validation

--- soldier_manager.py
+++ soldier_manager.py
@@ -1,5 +1,15 @@
+from until import find_soldier_by_id, is_valid_name
+from data import Soldiers
+
+
 def add_soldier(soldier_id: int, name: str):
-    pass
+        if find_soldier_by_id(soldier_id):
+             raise ValueError(f"ID number: {soldier_id} already exists in the system!")
+        elif is_valid_name(name):
+            raise ValueError(f"Invalid soldier name...")
+        else:
+            Soldiers.append({"id": soldier_id, "name": name, "duties": []})
+            print("Successfully added soldier")
 
 
 def remove_soldier(soldier_id: int):
