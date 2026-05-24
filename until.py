5af146f996a597c8800e92baa927a8e794bb1003
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-24 09:33:50 +0300

    Fix: correct remove_soldier logic and soldiers import case

--- until.py
+++ until.py
@@ -1,7 +1,7 @@
-from data import Soldiers
+from data import soldiers
 
 def find_soldier_by_id(soldier_id: int):
-    for soldier in Soldiers:
+    for soldier in soldiers:
         for id in soldier.values():
             if id == soldier_id:
                 return soldier
