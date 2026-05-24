6205ed7cef64f01b70d8fa28860e20462b634f71
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-24 11:51:49 +0300

    Adding a duty status update function

--- duty_manager.py
+++ duty_manager.py
@@ -15,8 +15,17 @@ def add_duty(soldier_id: int, duty_name: str, day: str):
 
 
 def update_duty_status(soldier_id: int, duty_name: str, new_status: str):
-    pass
-
+    soldier = u.find_soldier_by_id(soldier_id)
+    duty = u.find_duty_by_name(soldier["duties"], duty_name)
+    if not soldier:
+        raise KeyError(f"ID number: {soldier_id} does not exist in the system!")
+    if not duty:
+        raise ValueError("No duty was found for the requested soldier!")
+    if not u.is_valid_status(new_status):
+        raise ValueError("Invalid status!")
+    print(f"Status updated successfully! ({duty["status"]} -> {new_status})")
+    duty["status"] = new_status
+    
 
 def get_soldier_duties(soldier_id: int):
     pass
